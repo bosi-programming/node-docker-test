c1608e9b4cd3e4bba25b476a106fa6ac8ff59029
bosifullstack <bosiarquitetura@gmail.com>
2023-11-22 13:43:17 -0300

    UserEntity - getter tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,4 +20,25 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('Get name', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+  it('Get email', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+  it('Get password', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('Get createdAt', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
 });
